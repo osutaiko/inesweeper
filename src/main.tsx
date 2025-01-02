556573d55c3323f7eecd1dffa713452df6cb5ab7
osutaiko <henongod@gmail.com>
2025-01-02 18:18:05 +0900

    Replace Google Analytics with Vercel Analytics

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,11 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 
 import Layout from "./components/Layout";
+import { Analytics } from "@vercel/analytics/next";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <Layout />
+  <>
+    <Layout />
+    <Analytics />
+  </>
 );
