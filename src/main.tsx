179154ac5b210a702c022038411bbbad117b1fc9
osutaiko <henongod@gmail.com>
2025-01-04 12:42:46 +0900

    Use correct imports for Vercel Analytics and Speed Insights

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,8 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 
 import Layout from "./components/Layout";
-import { Analytics } from "@vercel/analytics/next";
-import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <>
