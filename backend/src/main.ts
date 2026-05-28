aeb3fcda2e235c66263c4dba45cde579698b0df9
Dongheon Han <henongod@gmail.com>
2026-05-28 15:27:45 +0900

    fix: CORS failure due to extra slash character in domain

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,8 +3,9 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const frontendUrl = process.env.FRONTEND_URL?.trim().replace(/\/$/, "");
   app.enableCors({
-    origin: process.env.FRONTEND_URL ?? 'http://localhost:3000',
+    origin: frontendUrl ?? 'http://localhost:3000',
     credentials: true,
   });
   await app.listen(process.env.PORT ?? 3001);
