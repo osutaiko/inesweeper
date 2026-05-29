e4a2eaae91e6d094dc6822eb4ab0b2fbd26b3231
Dongheon Han <henongod@gmail.com>
2026-05-29 17:38:18 +0900

    ui color scheme

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,13 +2,15 @@ import * as path from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import Unfonts from "unplugin-fonts/vite";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig(() => {
-  return {
-    plugins: [
-      react(),
-      Unfonts({
-        custom: {
+    return {
+      plugins: [
+        tailwindcss(),
+        react(),
+        Unfonts({
+          custom: {
           families: [
             {
               name: "Geist",
