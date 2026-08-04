5772496300cba662646f495c828af39482b7fe50
Dongheon Han <henongod@gmail.com>
2026-08-04 10:29:28 +0900

    feat: use compression for responses

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
+import compression from 'compression';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(compression());
   const frontendUrl = process.env.FRONTEND_URL?.trim().replace(/\/$/, "");
   app.enableCors({
     origin: frontendUrl ?? 'http://localhost:3000',
